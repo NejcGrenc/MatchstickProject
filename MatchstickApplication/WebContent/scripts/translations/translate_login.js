6ac06dd34b65b262196a53c690792b43bc41bd4d
Nejc <Nejc@DESKTOP-UOHP1VN>
2019-03-23 21:43:05 +0100

    revert translations for terms and agreement

--- MatchstickApplication/WebContent/scripts/translations/translate_login.js
+++ MatchstickApplication/WebContent/scripts/translations/translate_login.js
@@ -3,34 +3,25 @@ var translationMap =
 	'en': {
 		'm_welcome': 'Welcome',
 		'm_nameInput': 'Write your name',
-		'm_repeatedLogin': "Are you logging in for the first time?",
 		'm_acknowledgement' : 'By writing your name and pressing the green continue button, ' +
 							  '<br /> you agree to the <a id=\"button-agreement\" href="javascript:terms();">terms and conditions</a> of the study.'
 	},
 	'si': {
 		'm_welcome': 'Dobrodošli',
 		'm_nameInput': 'Vpišite svoje ime',
-		'm_repeatedLogin': "Ste se prijavili prvič?",
 		'm_acknowledgement' : 'By writing your name and pressing the green continue button, ' +
 		  					  '<br /> you agree to the <a id=\"button-agreement\" href="javascript:terms();">terms and conditions</a> of the study.'
 	},
 	'de': {
 		'm_welcome': 'Willcommen',
 		'm_nameInput': 'Write your creditcard number',
-		'm_repeatedLogin': "Are you logged in for the first time?",
-		'm_acknowledgement' : 'By writing your name and pressing the green continue button,' +
-							  '<br /> you agree to participate in the Matchstick study.' +
-							  '<br /> You allow us to collect and store annonymous personal data for track-keeping' +
-							  '<br /> and task related data (all activities done and times spent on the tasks).' +
-							  '<br /> Additionally you agree to have a cookie.'
+		'm_acknowledgement' : 'By writing your name and pressing the green continue button, ' +
+		  					  '<br /> you agree to the <a id=\"button-agreement\" href="javascript:terms();">terms and conditions</a> of the study.'
 	},
 	'sk': {
 		'm_welcome': 'Vitajte',
 		'm_nameInput': 'Napíšte Vaše meno',
-		'm_repeatedLogin': "Ste prihlásený po prvýkrát?",
-		'm_acknowledgement' : 'Napísaním Vášho mena a kliknutím na zelené tlačidlo Pokračovať,' +
-							  '<br /> Súhlasíte so zúčastnením na tejto štúdií.' +
-							  '<br /> Dovoľujete zhromaždiť a uchovať anonymné osobné údaje a údaje viazané k úlohám, t.j. všetky aktivity a čas strávený pri úlohách, pre ich použitie v tejto štúdií.' +
-							  '<br /> Taktiež súhlasíte s použitím cookies.'
+		'm_acknowledgement' : 'By writing your name and pressing the green continue button, ' +
+		  					  '<br /> you agree to the <a id=\"button-agreement\" href="javascript:terms();">terms and conditions</a> of the study.'
 	}
 }
