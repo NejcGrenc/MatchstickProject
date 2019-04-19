ab31468d08afa0947b06b2df41a7cfeae1d7e4e9
Nejc <Nejc@192.168.1.28>
2019-04-19 21:18:26 +0200

    create start-up servlet

---
+++ MatchstickApplication/src/main/java/grenc/masters/servlets/StartupServlet.java
@@ -0,0 +1,12 @@
+package grenc.masters.servlets;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet runs on Tomcat startup
+ */
+public class StartupServlet extends HttpServlet
+{
+	private static final long serialVersionUID = -3797587556764399582L;
+
+}
