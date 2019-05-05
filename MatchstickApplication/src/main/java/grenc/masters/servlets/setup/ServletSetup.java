fa76405ec3b112dcd686afa44998a87623ac7b64
Nejc <Nejc@192.168.1.29>
2019-05-05 23:35:50 +0200

    clean ServletSetup

--- MatchstickApplication/src/main/java/grenc/masters/servlets/setup/ServletSetup.java
+++ MatchstickApplication/src/main/java/grenc/masters/servlets/setup/ServletSetup.java
@@ -14,18 +14,14 @@ public class ServletSetup implements ServletContextListener {
 	@Override
     public void contextInitialized(ServletContextEvent event) {
         // Webapp startup.
-    	System.out.println("ServletSetup");
+    	System.out.println("MatchstickApplication startup!");
     	BeanProcessor.processPath("grenc.masters");
-    	System.out.println("ServletSetup--------");
-    	System.out.println();
-    	System.out.println();
-    	System.out.println("ServletSetup--------");
     }
 
 	@Override
     public void contextDestroyed(ServletContextEvent event) {
         // Webapp shutdown.
-    	System.out.println("Servlet shutdown --------");
+    	System.out.println("MatchstickApplication shutdown!");
     }
 
 }
