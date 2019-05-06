7da515d1b206e5dc4dcb06cb66d19149be8057fd
Nejc <Nejc@DESKTOP-UOHP1VN>
2019-05-06 14:46:59 +0200

    create ServletBeanProcessor

---
+++ MatchstickApplication/src/main/java/grenc/masters/servlets/bean/service/ServletBeanProcessor.java
@@ -0,0 +1,43 @@
+package grenc.masters.servlets.bean.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import grenc.masters.servlets.base.bean.ServletBean;
+import grenc.simpleton.Beans;
+import grenc.simpleton.annotation.Bean;
+
+
+@Bean
+public class ServletBeanProcessor
+{
+	private Map<String, ServletBean> servletBeans;
+	
+	public ServletBeanProcessor()
+	{
+		init();
+	}
+	
+	private void init()
+	{
+		this.servletBeans = new HashMap<String, ServletBean>();
+		for (Object bean : Beans.allRegisteredBeans())
+		{
+			if (bean instanceof ServletBean)
+			{
+				ServletBean servletBean = (ServletBean) bean;
+				this.servletBeans.put(servletBean.url(), servletBean);
+			}
+		}
+	}
+	
+	public ServletBean servletBeanByUrl(String url)
+	{
+		return servletBeans.get(url);
+	}
+	
+	public Map<String, ServletBean> allServletBeans()
+	{
+		return servletBeans;
+	}
+}
