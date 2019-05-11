384860878dcbff7874fa68205cd7c3e825e158a8
Nejc <Nejc@DESKTOP-UOHP1VN>
2019-05-11 17:26:22 +0200

    update runnersingle

--- MatchstickResolver/src/main/java/grenc/masters/matchstick/run/main/RunnerSingle.java
+++ MatchstickResolver/src/main/java/grenc/masters/matchstick/run/main/RunnerSingle.java
@@ -12,10 +12,10 @@ import grenc.masters.matchstick.run.filter.SolutionsFinder;
 
 public class RunnerSingle
 {
-	private static List<String> equation = Arrays.asList(" 2*3-3=5", "5/3+2=3", "3*5/5=2",
-													"5+5-5=3 ", "2/2*3=2", "2*5-5=3",
+	private static List<String> equation = Arrays.asList("2*3-3=5", "5/3+2=3", "3*5/5=2",
+													"5+5-5=3", "2/2*3=2", "2*5-5=3",
 													"5-5+2=3" , "3*2/3=3", "2*3-2=3",
-													" 3+3-5=5", "3*3-2=3 ", "2-5+2=5",
+													"3+3-5=5", "3*3-2=3", "2-5+2=5",
 													"2/5+5=2", "5/5+5=5", "2+5/2=5");
 	
 	public static void main(String[] args)
@@ -38,7 +38,7 @@ public class RunnerSingle
 		System.out.println("Belongs to group: " + group.name());
 		
 		for(EquationChangeSingle solution : correctFinalEquations)
-			System.out.println(" ----  " + solution.toString());
+			System.out.println("" + solution.toString());
 		
 		System.out.println();
 	}
